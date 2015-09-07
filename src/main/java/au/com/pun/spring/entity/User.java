9dfe3b111f2c2e3025d1fd953e1947cef054f041
IKEA <gitTest@local.com>
2015-09-08 00:23:33 +1000

    after:tutorial part 8

---
+++ src/main/java/au/com/pun/spring/entity/User.java
@@ -0,0 +1,24 @@
+package au.com.pun.spring.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue
+	private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	
+	
+}
